e1ee15d6270aee8fe725c1120a00693e6c91e89c
AlexisLeskinen <okaberintarou@yeah.net>
2019-11-18 21:09:02 +0800

    Start

---
+++ app/src/main/java/cn/finalHomework/EditTimeActivity.java
@@ -0,0 +1,14 @@
+package cn.finalHomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_time);
+    }
+}
