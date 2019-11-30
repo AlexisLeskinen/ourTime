950b78818d1e4057264c45b2f28cb40ce4f17da2
AlexisLeskinen <okaberintarou@yeah.net>
2019-11-30 22:58:35 +0800

    11.30添加主页布局的内容

--- app/src/main/java/cn/finalHomework/MainActivity.java
+++ app/src/main/java/cn/finalHomework/MainActivity.java
@@ -21,8 +21,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import cn.finalHomework.model.DataSource;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int requestCode = 1000;
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -31,21 +32,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        setStatusBarColor();
 
+        //顶部工具栏
         Toolbar toolbar = findViewById(R.id.main_top_bar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                Intent toAddEvent = new Intent(MainActivity.this, EditEventActivity.class);
-
-                startActivityForResult(toAddEvent, requestCode);
-            }
-        });
+        //左侧抽屉
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
@@ -64,13 +55,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    //设置状态栏颜色为透明
-    public void setStatusBarColor() {
-        Window window = getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
     }
 }
--- app/src/main/java/cn/finalHomework/ui/home/HomeFragment.java
+++ app/src/main/java/cn/finalHomework/ui/home/HomeFragment.java
@@ -1,27 +1,49 @@
 package cn.finalHomework.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import cn.finalHomework.EditEventActivity;
 import cn.finalHomework.R;
+import cn.finalHomework.model.DataSource;
 
 public class HomeFragment extends Fragment {
+    private static final int requestCode = 1000;
+    private DataSource appDAta;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        appDAta = new DataSource(getContext());
+        //读取保存的数据
+        appDAta.loadData();
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toAddEvent = new Intent(getContext(), EditEventActivity.class);
+                startActivityForResult(toAddEvent,requestCode);
+            }
+        });
 
         return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //保存数据
+        appDAta.saveData();
     }
 }
